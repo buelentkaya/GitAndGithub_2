95ecbb97597fa7b347ab2c28c8eab97a0e6bee25
Bulent Kaya <97361091+buelentkaya@users.noreply.github.com>
2022-04-08 18:17:02 +0200

    initial commit

---
+++ src/us100/Test002.java
@@ -0,0 +1,11 @@
+package us100;
+
+public class Test002 {
+    public static void main(String[] args) {
+        System.out.println("Please count");
+        for (int i = 0; i <10 ; i++) {
+            System.out.print(i+" ");
+        }
+
+    }
+}
