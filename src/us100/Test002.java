4abe1f836cb7adec9d9e1173000ff6d931ba0fd8
Bulent Kaya <97361091+buelentkaya@users.noreply.github.com>
2022-04-08 18:21:42 +0200

    3 commit

--- src/us100/Test002.java
+++ src/us100/Test002.java
@@ -8,5 +8,13 @@ public class Test002 {
         }
         System.out.println(" what now?");
         System.out.println(" what am I supposed to do right now,bro?");
+        System.out.println(" do you know multiplying?");
+        for (int i = 0; i < 10; i++) {
+           int x=5;
+
+           int result=x*i;
+                   System.out.println("result of 5 multiply with "+ i+ " equals "+result);
+        }
+
     }
 }
